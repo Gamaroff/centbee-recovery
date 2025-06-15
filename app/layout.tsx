0680e4e5415b6192d909493883834414f93f3729
rjseibane <rjseibane@gmail.com>
2025-06-15 16:41:10 +0200

    Remove unused component

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/sonner"
 
 export const metadata: Metadata = {
   title: {
